52d71a925268f9756d92944ab8bc41b33c4df2bc
Ayaskanta Samal <ayaskantasamal@Ayaskantas-MacBook-Pro.local>
2021-12-29 19:33:15 -0500

    Docker file added

--- api/main.py
+++ api/main.py
@@ -2,16 +2,19 @@ import os
 import requests
 from flask import Flask,request
 from dotenv import load_dotenv
+from flask_cors import CORS,cross_origin
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_KEY =os.environ.get("UNSPLASH_KEY","")
+UNSPLASH_KEY =os.environ.get("UNSPLASH_KEY","abc")
 UNSPLASH_URL ="https://api.unsplash.com/photos/random";
+DEBUG =bool(os.environ.get("DEBUG",False))
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
 
 app = Flask(__name__)
-
+CORS(app)
+app.config["DEBUG"] =DEBUG
 @app.route("/")
 def hello():
     return "Hello, World!"
